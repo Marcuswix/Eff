82b791112481a39f3a74cf9bdffc0845b491c5dd
Marcus <Marcuswix@yahoo.se>
2023-10-10 15:02:45 +0200

    Har lagt till en 'Till toppen'-knapp

--- js/script.js
+++ js/script.js
@@ -11,3 +11,30 @@ overlay.addEventListener('click', () => {
   menu.classList.remove('open')
   overlay.classList.remove('show')
 })
+
+// "To the Top" - button
+function toTheTop()
+{
+  window.scrollTo(
+    {
+      top: 0,
+      behavior: "smooth"
+    }
+  )
+}
+
+// Hide "To the Top" - button
+window.addEventListener("scroll", function()
+{
+  let element = document.getElementById("btn-top")
+
+  if (window.scrollY > 100)
+  {
+    element.classList.remove("btn-top-hide")
+  }
+
+  else
+  {
+    element.classList.add("btn-top-hide")
+  }
+})
